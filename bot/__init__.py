ccca9c70993aac22e9cdc886ee0e6f7b26349477
Abhyuday Pratap Singh <76898190+Abhyudday@users.noreply.github.com>
2025-08-18 03:13:35 +0530

    Fix missing create_dispatcher import in bot module __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,6 +1,6 @@
 """Telegram bot package."""
 
-from .bot import create_bot, setup_handlers
+from .bot import create_bot, setup_handlers, create_dispatcher
 from .handlers import user_handlers, admin_handlers
 from .keyboards import get_main_keyboard, get_admin_keyboard
 from .utils import format_wallet_address, format_number, is_admin
@@ -8,6 +8,7 @@ from .utils import format_wallet_address, format_number, is_admin
 __all__ = [
     "create_bot", 
     "setup_handlers", 
+    "create_dispatcher",
     "user_handlers", 
     "admin_handlers",
     "get_main_keyboard", 
